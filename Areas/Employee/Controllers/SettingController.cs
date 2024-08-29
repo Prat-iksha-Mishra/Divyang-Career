6ca5f8fb58b645aa2cc114983b5baecdab246a49
sugamsharmaIt <sugamsharma0007@gmail.com>
2024-08-29 17:54:42 +0530

    Add Setting page in Employer by sugam sharma

---
+++ Areas/Employee/Controllers/SettingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DivyangPortalWeb.Areas.Employee.Controllers
+{
+    public class SettingController : Controller
+    {
+        // GET: Employee/Setting
+        public ActionResult Setting()
+        {
+            return View();
+        }
+    }
+}
--- Areas/Employee/Scripts/js/min.js
+++ Areas/Employee/Scripts/js/min.js
@@ -83,39 +83,111 @@ jQuery(document).ready(function ($) {
         console.error(error);
     });
 
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 
     /*upload image*/
 
-    var btn_save = $(".save-profile"),
-        btn_edit = $(".edit-profile"),
-        img_object = $(".img-object"),
-        overlay = $(".media-overlay"),
-        media_input = $("#media-input");
+    $(document).ready(function () {
+        var fileArr = [];
+        $("#images").change(function () {
+            if (fileArr.length > 0) fileArr = [];
 
-    btn_save.hide(0);
-    overlay.hide(0);
+            $('#image_preview').html("");
+            var total_file = document.getElementById("images").files;
+            if (!total_file.length) return;
+            for (var i = 0; i < total_file.length; i++) {
+                if (total_file[i].size > 1048576) {
+                    return false;
+                } else {
+                    fileArr.push(total_file[i]);
+                    $('#image_preview').append("<div class='img-div' id='img-div" + i + "'><img src='" + URL.createObjectURL(event.target.files[i]) + "' class='image' title='" + total_file[i].name + "'><div class='middle'><button id='action-icon' value='img-div" + i + "' class='btn btn-danger' role='" + total_file[i].name + "'><i class='fa fa-trash'></i></button></div></div>");
+                }
+            }
+        });
 
-    btn_edit.on("click", function () {
-        $(this).hide(0);
-        overlay.fadeIn(300);
-        btn_save.fadeIn(300);
-    });
-    btn_save.on("click", function () {
-        $(this).hide(0);
-        overlay.fadeOut(300);
-        btn_edit.fadeIn(300);
-    });
+        $('body').on('click', '#action-icon', function (evt) {
+            var divName = this.value;
+            var fileName = $(this).attr('role');
+            $(`#${divName}`).remove();
 
-    media_input.change(function () {
-        if (this.files && this.files[0]) {
-            var reader = new FileReader();
+            for (var i = 0; i < fileArr.length; i++) {
+                if (fileArr[i].name === fileName) {
+                    fileArr.splice(i, 1);
+                }
+            }
+            document.getElementById('images').files = FileListItem(fileArr);
+            evt.preventDefault();
+        });
 
-            reader.onload = function (e) {
-                img_object.attr("src", e.target.result);
-            };
 
-            reader.readAsDataURL(this.files[0]);
-        }
+
+        var fileArr = [];
+        $("#images2").change(function () {
+            if (fileArr.length > 0) fileArr = [];
+
+            $('#image_preview2').html("");
+            var total_file = document.getElementById("images2").files;
+            if (!total_file.length) return;
+            for (var i = 0; i < total_file.length; i++) {
+                if (total_file[i].size > 1048576) {
+                    return false;
+                } else {
+                    fileArr.push(total_file[i]);
+                    $('#image_preview2').append("<div class='img-div2' id='img-div2" + i + "'><img src='" + URL.createObjectURL(event.target.files[i]) + "' class='image2' title='" + total_file[i].name + "'><div class='middle2'><button id='action-icon2' value='img-div2" + i + "' class='btn btn-danger' role='" + total_file[i].name + "'><i class='fa fa-trash'></i></button></div></div>");
+                }
+            }
+        });
+
+        $('body').on('click', '#action-icon2', function (evt) {
+            var divName = this.value;
+            var fileName = $(this).attr('role');
+            $(`#${divName}`).remove();
+
+            for (var i = 0; i < fileArr.length; i++) {
+                if (fileArr[i].name === fileName) {
+                    fileArr.splice(i, 1);
+                }
+            }
+            document.getElementById('images2').files = FileListItem(fileArr);
+            evt.preventDefault();
+        });
+
+
+        var fileArr = [];
+        $("#images3").change(function () {
+            if (fileArr.length > 0) fileArr = [];
+
+            $('#image_preview3').html("");
+            var total_file = document.getElementById("images3").files;
+            if (!total_file.length) return;
+            for (var i = 0; i < total_file.length; i++) {
+                if (total_file[i].size > 1048576) {
+                    return false;
+                } else {
+                    fileArr.push(total_file[i]);
+                    $('#image_preview3').append("<div class='img-div3' id='img-div3" + i + "'><img src='" + URL.createObjectURL(event.target.files[i]) + "' class='image3' title='" + total_file[i].name + "'><div class='middle3'><button id='action-icon3' value='img-div3" + i + "' class='btn btn-danger' role='" + total_file[i].name + "'><i class='fa fa-trash'></i></button></div></div>");
+                }
+            }
+        });
+
+        $('body').on('click', '#action-icon3', function (evt) {
+            var divName = this.value;
+            var fileName = $(this).attr('role');
+            $(`#${divName}`).remove();
+
+            for (var i = 0; i < fileArr.length; i++) {
+                if (fileArr[i].name === fileName) {
+                    fileArr.splice(i, 1);
+                }
+            }
+            document.getElementById('images3').files = FileListItem(fileArr);
+            evt.preventDefault();
+        });
+
+        
     });
 
 
