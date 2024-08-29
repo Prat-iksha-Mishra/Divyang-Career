261b2ae6b0c7d3806b161276ce1569618dab81ac
sugamsharmaIt <sugamsharma0007@gmail.com>
2024-08-29 17:02:03 +0530

    add new controller for employer dashboar by sugam

---
+++ Areas/Employee/Controllers/CompaniesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DivyangPortalWeb.Areas.Employee.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        // GET: Employee/Companies
+        public ActionResult Companies()
+        {
+            return View();
+        }
+        public ActionResult Companiesdetails()
+        {
+            return View();
+        }
+
+        public ActionResult addnewcompany()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Employee/Controllers/applicantsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DivyangPortalWeb.Areas.Employee.Controllers
+{
+    public class applicantsController : Controller
+    {
+        // GET: Employee/applicants
+        public ActionResult applicants()
+        {
+            return View();
+        }
+    }
+}
